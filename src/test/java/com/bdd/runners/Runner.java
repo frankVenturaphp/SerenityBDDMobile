5f93f33f3f616a1da38208f6295def1020419c50
Eduardo <evictorio.uni@gmail.com>
2022-07-03 02:26:02 -0500

    I upload changes from Eduardo Victorio

--- src/test/java/com/bdd/runners/Runner.java
+++ src/test/java/com/bdd/runners/Runner.java
@@ -1,5 +1,6 @@
 package com.bdd.runners;
 
+import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
